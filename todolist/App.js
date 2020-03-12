2e0fe300d99682b80b9edd20aa09aacfbb3c3ee3
LNPappas <LPappas@gmail.com>
2020-03-12 15:37:09 -0700

    init addtodo

--- todolist/App.js
+++ todolist/App.js
@@ -2,25 +2,34 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoitem'; 
+import AddTodo from './components/addtodo';
 
 export default function App() {
   const [todos, setTodos] = useState([
-    { text: 'buy coffee', key: '1'},
-    { text: 'buy milk', key: '2'},
-    { text: 'buy bread', key: '3'},
+    { text: 'test todo list', key: '1'},
   ]);
 
   const pressHandler = (key) => {
     setTodos((prevTodos) => {
-      return prevTodos.filter(todo => todo.key != key)
+      return prevTodos.filter(todo => todo.key != key);
+    });
+  }
+
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [
+        { text: text, key: Math.random.toString() },
+        ...prevTodos
+      ];
     })
+
   }
 
   return (
     <View style={styles.container}>
-      {/* header */}
+      <Header />
       <View style={styles.content}>
-        <Header />
+        <AddTodo submitHandler={submitHandler} />
         <View style={styles.list}>
           <FlatList
             data={todos}
